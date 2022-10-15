a81d947104b5ad2b5938c9571e072886ec61f1e0
Joseph Guhlin <joseph.guhlin@gmail.com>
2022-10-15 19:35:47 +1300

    Update readme

--- basecall.sh
+++ basecall.sh
@@ -1,1 +1,1 @@
-bonito basecaller dna_r9.4.1_e8_sup@v3.3 ~/stonefly/all_fast5/ --batchsize 384 --reference index.mmi --save-ctc --recursive --device "cuda:0" --alignment-threads 16 | samtools view -S -b - > basecalled-default-model/basecalls.bam
+bonito basecaller dna_r9.4.1_e8_sup@v3.3 ~/stonefly/all_fast5/ --batchsize 384 --reference assembly.fasta --save-ctc --recursive --device "cuda:0" --alignment-threads 16 > basecalled-default-model/basecalls.bam
