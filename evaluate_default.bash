602c5abe7b155019fa4784a1f88fb0eaabbb6161
Joseph Guhlin <joseph.guhlin@gmail.com>
2022-11-04 08:06:40 +1300

    Updates

---
+++ evaluate_default.bash
@@ -0,0 +1,5 @@
+bonito evaluate \
+	--batch 36 \
+	--directory basecalled-default-model \
+	--weights 1 \
+	/home/josephguhlin/.asdf/installs/python/anaconda3-2021.05/envs/bonito/lib/python3.8/site-packages/bonito/models/dna_r9.4.1_e8_sup@v3.3
--- training_nopretrained.bash
+++ training_nopretrained.bash
@@ -2,6 +2,5 @@ bonito train -f \
 	--restore-optim \
 	--batch 36 \
 	--epochs 96 --lr 1e-5 \
-	--pretrained dna_r9.4.1_e8_sup@v3.3 \
 	--directory basecalled-default-model \
 	./aptera_tuned_nopretrained
